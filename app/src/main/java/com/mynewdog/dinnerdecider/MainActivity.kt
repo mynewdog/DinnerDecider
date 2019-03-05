8d410f94c1622fe2d7ea304d18418305d29e3fe6
mynewdog <erivalde@hotmail.com>
2019-03-05 18:40:27 +0100

    Simple app that decides food for you

---
+++ app/src/main/java/com/mynewdog/dinnerdecider/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.mynewdog.dinnerdecider
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Noodles", "Pizza")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        decideBtn.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodTxt.text = foodList[randomFood]
+        }
+
+        addFoodBtn.setOnClickListener {
+            val newFood = addFoodTxt.text.toString()
+            foodList.add(newFood)
+            addFoodTxt.text.clear()
+            println(foodList)
+
+        }
+
+    }
+}
